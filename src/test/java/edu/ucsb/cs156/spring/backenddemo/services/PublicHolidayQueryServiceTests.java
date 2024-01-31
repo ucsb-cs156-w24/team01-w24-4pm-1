1413f419895d0bb8c962f916e8c36f719ab29483
Chris Riney <chrisriney@169-231-36-96.wireless.ucsb.edu>
2024-01-30 17:40:26 -0800

    Created PublicHolidayController.java and test

--- src/test/java/edu/ucsb/cs156/spring/backenddemo/services/PublicHolidayQueryServiceTests.java
+++ src/test/java/edu/ucsb/cs156/spring/backenddemo/services/PublicHolidayQueryServiceTests.java
@@ -23,7 +23,7 @@ public class PublicHolidayQueryServiceTests {
     @Test
     public void test_getJSON() {
         String year = "2011";
-        String countryCode = "U.S.A.";
+        String countryCode = "001";
 
         String expectedURL = PublicHolidayQueryService.ENDPOINT.replace("{year}", year).replace("{countryCode}", countryCode);
 
